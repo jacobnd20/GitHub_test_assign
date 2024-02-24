51cf70f3cbd3401b6b6b29105a89cc192b3b9056
Jacob Daines <10853352@uvu.edu>
2024-02-23 21:27:42 -0700

    Assing nav

--- streamlit_template/main_page.py
+++ streamlit_template/main_page.py
@@ -4,3 +4,6 @@ st.markdown("# Welcome to my Website!")
 st.sidebar.markdown("# Main Page")
 
 st.write("Click on a page to see racer stats")
+
+link = '(To my Github Pages Site)(https://jacobnd20.github.io/GitHub_test_assign/)'
+st.markdown(link, unsafe_allow_html=True)
